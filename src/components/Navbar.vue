dad2a6b42e4cd95391a58292720fdd4e2fc96e00
Samuel Muli <samuelmuli95@gmail.com>
2024-05-16 13:36:26 +0300

    Add all button

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -8,6 +8,7 @@
 			<RouterLink :to="{name: 'home'}" class="text-3xl font-bold uppercase text-gray-700">Gazetify</RouterLink>
 			<div class="flex items-center gap-10">
 				<div class="flex gap-4">
+					<button class="border-2 border-gray-800 hover:bg-gray-800 px-3 py-2 rounded hover:text-white">All</button>
 					<button class="border-2 border-red-300 hover:bg-red-300 px-3 py-2 rounded">Politics</button>
 					<button class="border-2 border-blue-700 hover:bg-blue-700 px-3 py-2 rounded hover:text-white">Sports</button>
 					<button class="border-2 border-gray-800 hover:bg-gray-800 px-3 py-2 rounded hover:text-white">Weather</button>
